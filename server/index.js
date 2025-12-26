8cce706dd1b0011f3a2936cedd58ebb6cf89716f
Koustav Halder <koustavhalder@Koustavs-MacBook-Air.local>
2025-12-27 00:47:34 +0530

    CORS error fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API is running!!");
